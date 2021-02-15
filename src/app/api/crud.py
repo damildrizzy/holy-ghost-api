e4a1cf645aba73093f9824b936a8d2a095a70283
samuel adekoya <damiloladrizzy@yahoo.com>
2021-02-15 22:19:19 +0100

    update users crud

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -1,25 +1,37 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 
 from . import schemas
-from app import models
+from app.models import User
+from app.security import get_password_hash, verify_password
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    return db.query(User).filter(User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+    return db.query(User).filter(User.email == email).first()
 
 
 def get_users(db: Session):
-    return db.query(models.User).all()
+    return db.query(User).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+def create_user(db: Session, user: schemas.UserCreate) -> User:
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def authenticate(db: Session, email: str, password: str) -> Optional[User]:
+    user = get_user_by_email(email=email)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
