31ef2657d4db8ecf5e2fe62405b3a0105e5b40a2
samuel adekoya <damiloladrizzy@yahoo.com>
2021-02-18 22:54:19 +0100

    black doing it thing

--- src/app/security.py
+++ src/app/security.py
@@ -20,15 +20,12 @@ def get_password_hash(plain_password):
     return pwd_context.hash(plain_password)
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None
-                        ) -> str:
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-            )
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
     return encoded_jwt
