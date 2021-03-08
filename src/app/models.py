61819d5f8157a8a51f3fac8912e8e4164f6736cf
samuel adekoya <damiloladrizzy@yahoo.com>
2021-03-08 21:56:26 +0100

    Tongue model

--- src/app/models.py
+++ src/app/models.py
@@ -10,3 +10,11 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+
+class Tongue(Base):
+    __tablename__ = "tongues"
+
+    id = Column(Integer, primary_key=True, index=True)
+    raw_string = Column(String)
+    tongues_string = Column(String)
