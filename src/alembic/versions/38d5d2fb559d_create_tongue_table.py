aaae8fcf06a2c827479c09f59532aebc5d535649
samuel adekoya <damiloladrizzy@yahoo.com>
2021-03-10 04:23:51 +0100

    alembic migration

---
+++ src/alembic/versions/38d5d2fb559d_create_tongue_table.py
@@ -0,0 +1,24 @@
+"""create tongue table
+
+Revision ID: 38d5d2fb559d
+Revises: f61307fd59e6
+Create Date: 2021-03-10 04:22:44.373006
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38d5d2fb559d'
+down_revision = 'f61307fd59e6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
